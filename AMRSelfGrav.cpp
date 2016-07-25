2d8389b7c8d1f9294ba42253c46a15688ebcfd09
hbaehr <baehr7@gmail.com>
2016-07-25 12:29:11 +0200

    Starting working file: where is all begins

---
+++ AMRSelfGrav.cpp
@@ -0,0 +1,48 @@
+#ifdef CH_LANG_CC
+/*
+ *      _______              __
+ *     / ___/ /  ___  __ _  / /  ___
+ *    / /__/ _ \/ _ \/  V \/ _ \/ _ \
+ *    \___/_//_/\___/_/_/_/_.__/\___/
+ *    Please refer to Copyright.txt, in Chombo's root directory.
+ */
+#endif
+
+#include <iostream>
+using std::ifstream;
+using std::ios;
+
+// #define HALEM_PROC_SPEED
+#ifdef HALEM_PROC_SPEED
+#include <cstdio>
+#include <sys/sysinfo.h>
+#include <machine/hal_sysinfo.h>
+#endif
+
+#ifdef CH_MPI
+#include "CH_Attach.H"
+#endif
+
+#include "FABView.H"
+
+#include "ParmParse.H"
+#include "CH_HDF5.H"
+#include "parstream.H"
+#include "CH_Timer.H"
+#include "memusage.H"
+
+#include "AMR.H"
+#include "AMRLevelPlutoFactory.H"
+
+#include "UsingNamespace.H"
+
+#ifdef CH_LINUX
+// Should be undefined by default
+#define TRAP_FPE
+#undef  TRAP_FPE
+#endif
+
+#ifdef TRAP_FPE
+static void enableFpExceptions();
+#endif
+
