a1ca134ad2a6e32b2d106e5f172fb33ccd44142c
hbaehr <baehr7@gmail.com>
2016-07-26 13:57:48 +0200

    Cleaned up old attempts, start working with the example in the documentation.

--- AMRSelfGrav.cpp
+++ AMRSelfGrav.cpp
@@ -34,6 +34,7 @@ using std::ios;
 #include "AMR.H"
 #include "AMRLevelPlutoFactory.H"
 #include "AMRPoissonOp.H"
+#include "AMRMultiGrid.H"
 
 #include "UsingNamespace.H"
 
@@ -46,45 +47,8 @@ using std::ios;
 #ifdef TRAP_FPE
 static void enableFpExceptions();
 #endif
-
-/* List of things which need to happen
- *
- * Calculate self-gravitational potential
- * Calculate aceleration due to self-gravity
- * Export calculations to main part of PLUTO
- *  - Store calculations somewhere (temporarily)?
- * 
- *
- */
     
-// Use CHOMBO functions to solve for the self-gravitational potential
-    
-// Define a host of PoissonOps for all necessary levels
-void AMRPoissonOpFactory::define(a_coarseDomain   // domain at the coarsest level
-                                 a_grids          // AMR heirarchy
-                                 a_refRatios      // refinement ratios between levels
-                                 a_coarsedx       // grid spacing at the coarsest level
-                                 a_bc             // boundary conditions
-                                 a_alpha=0.0      // identity coefficient = 0
-                                 a_beta=1.0       // Lapacian coefficient = 1/(4*pi*G)
-                            );
-
-//
-void AMRPoissonOp::(
-                    );
-
-//
-virtual void AMRPoissonOp::applyOp(
-                                   );
-
-// Getting an accurate value
-virtual void AMRPoissonOp::residual(LevelData<FArrayBox>&  	a_lhs,      // left-hand side
-                                    const LevelData<FArrayBox>&  a_phi, // self-gravitational potential
-                                    const LevelData<FArrayBox>&  a_rhs, // right-hand side (rho)
-                                    bool  a_homogeneous = false         // 
-                                    );
-
-// Example from the documentation
+// Example from the Chombo documentation
 
 /*
  * solveElliptic solves (alpha I + beta Laplacian) phi = rhs
@@ -101,14 +65,14 @@ virtual void AMRPoissonOp::residual(LevelData<FArrayBox>&  	a_lhs,      // left-
  * phi = (alpha I + beta Lapl)^{-1}(rhs)
  */
 
-void solveElliptic(Vector<LevelData<FArrayBox>* >& phi,
-                   const Vector<LevelData<FArrayBox>* > rhs,
-                   const Vector<DisjointBoxLayout>& grids,
-                   const Vector<int>& refRatios,
-                   const ProblemDomain& level0Domain,
-                   Real alpha, Real beta, Real coarsestDx)
+void solveElliptic(Vector<LevelData<FArrayBox>* >& phi,         // Output self-gravity potential
+                   const Vector<LevelData<FArrayBox>* > rhs,    // Input density 
+                   const Vector<DisjointBoxLayout>& grids,      // Grid geometries
+                   const Vector<int>& refRatios,                // Vector defining refinement ratios between levels
+                   const ProblemDomain& level0Domain,           // 
+                   Real alpha, Real beta, Real coarsestDx)      // constants alpha=0, beta=1/(4*pi*G)
 {
-int numlevels = rhs.size();
+int numlevels = rhs.size(); // A different array for each refinement level
 
 //define the operator factory
 AMRPoissonOpFactory opFactory;
@@ -134,12 +98,6 @@ int lbase = 0;
 //so solve already.
 solver.solve(phi, rhs, numlevels-1, lbase);
 }
-
-// Calculate physics of self-gravity grad(phi) = g (Do this here or in the main PLUTO code?)
-// It might be useful to do it here because Chombo should have no issues doing the gradient 
-// over grid refinement levels
-
-// Export calculations to the main part of PLUTO (phi and grad(phi)?)
  
 // Fluxes?
 
