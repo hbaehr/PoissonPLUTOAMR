3718a4bce7716959d77131cc08a2700c7c9123dc
hbaehr <baehr7@gmail.com>
2017-08-25 12:08:26 +0200

    Added the grid generation part, may not be necessary, and comments will come later.

--- AMRSelfGrav.cpp
+++ AMRSelfGrav.cpp
@@ -567,6 +567,302 @@ void setRHS(Vector<LevelData<FArrayBox>* > a_rhs,                               
  }
 
 
+void setupGrids(Vector<DisjointBoxLayout>& a_amrGrids,                           // uncertain whether I will need to setup my own grid, but
+            Vector<ProblemDomain>& a_amrDomains,                                 // this will be good for completeness sake
+            Vector<int>& a_refRatios,
+            Vector<Real>& a_amrDx,
+            int& a_finestLevel)
+{
+   CH_TIME("setupGrids");
+
+   a_finestLevel = 0;
+   ParmParse ppGrids("grids");
+
+   // get grid generation parameters
+   int maxLevel, maxBoxSize, blockFactor;                                        // parameters to be read from file
+   Real fillRatio;
+
+   ppGrids.get("max_level", maxLevel);
+
+   ppGrids.get("max_box_size",maxBoxSize);
+
+   ppGrids.get("block_factor", blockFactor);
+
+   ppGrids.get("fillRatio", fillRatio);
+
+   // note that there only need to be numLevels-1 refinement ratios
+   a_refRatios.resize(maxLevel);
+   ppGrids.getarr("ref_ratio", a_refRatios, 0, maxLevel);                        // read the refinement ratios as an array
+
+   Vector<int>  is_periodic_int;                                                 // periodicity of the boundaries in each direction
+   bool is_periodic[SpaceDim];
+   ppGrids.getarr("is_periodic", is_periodic_int, 0, SpaceDim);
+   for (int dir=0; dir<SpaceDim; dir++)
+     {
+       is_periodic[dir] = (is_periodic_int[dir] == 1);
+     }
+
+   IntVect numCells;
+   Vector<int> incells(SpaceDim);
+   ppGrids.getarr("num_cells", incells, 0, SpaceDim);
+   numCells = IntVect(D_DECL6(incells[0],incells[1],incells[2],
+                              incells[3],incells[4],incells[5]) );
+
+   RealVect domainSize = RealVect::Unit;
+   if (ppGrids.contains("domain_size"))
+     {
+       Vector<Real> insize(SpaceDim);
+       ppGrids.getarr("domain_size", insize, 0, SpaceDim);
+       domainSize = RealVect(D_DECL6(insize[0],insize[1],insize[2],
+                               insize[3],insize[4],insize[5]) );
+     }
+
+   // resize dataholders
+   int maxNumLevels = maxLevel +1;
+   a_amrGrids.resize(maxNumLevels);
+   a_amrDomains.resize(maxNumLevels);
+   a_amrDx.resize(maxNumLevels,-1);
+   a_finestLevel = 0;
+
+   // assumes dx=dy=dz
+   a_amrDx[0] = domainSize[0]/numCells[0];
+
+   IntVect domLo = IntVect::Zero;
+   IntVect domHi  = numCells - IntVect::Unit;
+
+   ProblemDomain baseDomain(domLo, domHi, is_periodic);
+   a_amrDomains[0] = baseDomain;
+
+   // set up refined domains, etc
+   for (int lev=1; lev<= maxLevel; lev++)
+     {
+       a_amrDomains[lev] = a_amrDomains[lev-1];
+       a_amrDomains[lev].refine(a_refRatios[lev-1]);
+       a_amrDx[lev] = a_amrDx[lev-1]/a_refRatios[lev-1];
+     }
+
+   Vector<Vector<Box> > vectBoxes(maxLevel+1);
+
+   // local scope. for base-level grid generation
+   {
+     CH_TIME("BaseGridCreation");
+     // generate base level grids
+
+     domainSplit(baseDomain, vectBoxes[0], maxBoxSize, blockFactor);
+
+     Vector<int> procAssign(vectBoxes[0].size(), 0);
+
+     LoadBalance(procAssign, vectBoxes[0]);
+
+     DisjointBoxLayout baseGrids(vectBoxes[0], procAssign, baseDomain);
+
+     a_amrGrids[0] = baseGrids;
+   }
+
+
+   if (maxLevel > 0)
+     {
+       bool read_grids = false;
+       ppGrids.query("read_in_grids", read_grids);
+       if (read_grids)
+         {
+           for (int ilev = 1; ilev <= maxLevel; ilev++)
+             {
+               const ProblemDomain& levDomain = a_amrDomains[ilev];
+
+               Vector<Box>   boxes;
+               char boxCountVar[100];
+               int boxCount;
+               sprintf(boxCountVar, "level_%d_box_count", ilev);
+               ppGrids.get(boxCountVar, boxCount);
+               boxes.resize(boxCount);
+               for (int ibox = 0; ibox < boxCount; ibox++)
+                 {
+                   char boxLoVar[100];
+                   char boxHiVar[100];
+                   sprintf(boxLoVar, "level_%d_box_%d_lo", ilev, ibox);
+                   sprintf(boxHiVar, "level_%d_box_%d_hi", ilev, ibox);
+                   Vector<int> boxLo, boxHi;
+                   ppGrids.getarr(boxLoVar, boxLo, 0, SpaceDim);
+                   ppGrids.getarr(boxHiVar, boxHi, 0, SpaceDim);
+                   IntVect ivLo(D_DECL(boxLo[0], boxLo[1], boxLo[2]));
+                   IntVect ivHi(D_DECL(boxHi[0], boxHi[1], boxHi[2]));
+                   boxes[ibox] = Box(ivLo, ivHi);
+                   if (!levDomain.contains(boxes[ibox]))
+                     {
+                       MayDay::Error("box outside of domain");
+                     }
+                 }
+               //check to see if level 0 domain is covered
+               if (ilev == 0)
+                 {
+                   IntVectSet ivDom(levDomain.domainBox());
+                   for (int ibox = 0; ibox < boxes.size(); ibox++)
+                     {
+                       ivDom -= boxes[ibox];
+                     }
+                   if (!ivDom.isEmpty())
+                     {
+                       MayDay::Error("level 0 boxes must cover the domain");
+                     }
+                 }
+               Vector<int>  proc(boxes.size());
+               LoadBalance(proc,boxes);
+               a_amrGrids[ilev] = DisjointBoxLayout(boxes, proc, levDomain);
+               a_finestLevel++;
+             }
+
+         }
+       else
+         {
+           // tag on grad(rhs)
+           int bufferSize = 1;
+           BRMeshRefine meshGen(a_amrDomains[0],
+                                a_refRatios,
+                                fillRatio,
+                                blockFactor,
+                                bufferSize,
+                                maxBoxSize);
+
+           // to be used by MeshRefine...
+           Vector<Vector<Box> > oldMeshes(maxLevel+1);
+           oldMeshes[0] = vectBoxes[0];
+           for (int lev=1; lev<oldMeshes.size(); lev++)
+             {
+               oldMeshes[lev].push_back(a_amrDomains[lev].domainBox());
+             }
+
+           Real refineThresh;
+           ppGrids.get("refine_threshold", refineThresh);
+
+           Real threshSqr = refineThresh*refineThresh;
+
+           bool moreLevels = true;
+           while (moreLevels)
+             {
+               // tag based on grad(rhs)
+               // first need to allocate RHS
+               Vector<LevelData<FArrayBox>* > tempRHS(a_finestLevel+1, NULL);
+               for (int lev=0; lev<= a_finestLevel; lev++)
+                 {
+                   // note that we add a ghost cell to simplify gradients
+                   tempRHS[lev] = new LevelData<FArrayBox>(a_amrGrids[lev],
+                                                           1, IntVect::Unit);
+                 }
+
+               setRHS(tempRHS, a_amrDomains, a_refRatios, a_amrDx,
+                      a_finestLevel);
+
+               Vector<IntVectSet> tags(a_finestLevel+1);
+
+               for (int lev=0; lev<a_finestLevel+1; lev++)
+                 {
+                   const DisjointBoxLayout& levelGrids = a_amrGrids[lev];
+                   const LevelData<FArrayBox>& levelRHS = *tempRHS[lev];
+                   IntVectSet& levelTags = tags[lev];
+
+                   // compute mag(gradient)
+                   DataIterator dit = levelGrids.dataIterator();
+                   for (dit.begin(); dit.ok(); ++dit)
+                     {
+                       const FArrayBox& rhsFab = levelRHS[dit];
+                       // local storage foer gradient
+                       FArrayBox gradFab(levelGrids[dit],1);
+                       gradFab.setVal(0.0);
+                       Real thisGrad;
+
+                       BoxIterator bit(levelGrids[dit]);
+                       for (bit.begin(); bit.ok(); ++bit)
+                         {
+                           IntVect iv=bit();
+                           for (int dir=0; dir<SpaceDim; dir++)
+                             {
+                               // use mag(undivided gradient)
+                               IntVect hi = iv + BASISV(dir);
+                               IntVect lo = iv - BASISV(dir);
+                               thisGrad = rhsFab(hi,0) - rhsFab(lo,0);
+                               gradFab(iv,0) += (thisGrad*thisGrad);
+                             } // end loop over directions
+                         } // end loop over cells
+
+                       //gradFab now has mag(grad*dx)^2
+
+                       // tag where mag(gradient) > tolerance^2
+                       for (bit.begin(); bit.ok(); ++bit)
+                         {
+                           IntVect iv = bit();
+                           if (gradFab(iv,0) > threshSqr)
+                             {
+                               levelTags |= iv;
+                             }
+                         } // end loop over cells
+                     } // end loop over grids on this level
+
+                 } // end loop over levels
+
+
+               // call meshRefine.
+               for (int lev=1; lev<=a_finestLevel; lev++)
+                 {
+                   oldMeshes[lev] = vectBoxes[lev];
+                 }
+
+               int topLevel = a_finestLevel;
+               int newFinestLevel =  meshGen.regrid(vectBoxes,
+                                                    tags,
+                                                    0,
+                                                    topLevel,
+                                                    oldMeshes);
+
+
+               // define new grids if necessary and test to see if we're done
+               if (newFinestLevel > a_finestLevel)
+                 {
+                   a_finestLevel = newFinestLevel;
+
+                   // setup new grid hierarchy
+                   for (int lev=1; lev<=a_finestLevel; lev++)
+                     {
+                       Vector<int> procAssign(vectBoxes[lev].size(),0);
+                       LoadBalance(procAssign, vectBoxes[lev]);
+                       DisjointBoxLayout levelGrids(vectBoxes[lev],
+                                                    procAssign,
+                                                    a_amrDomains[lev]);
+                       a_amrGrids[lev] = levelGrids;
+                     }
+                   if (s_verbosity>2) pout() << "setupGrids: "<< a_finestLevel <<") size " << a_amrGrids[a_finestLevel].size() << endl;
+                 }
+               else
+                 {
+                   moreLevels = false;
+                 }
+
+               if (a_finestLevel == maxLevel)
+                 {
+                   moreLevels = false;
+                 }
+
+               // clean up before starting again
+               for (int lev=0; lev<tempRHS.size(); lev++)
+                 {
+                   delete tempRHS[lev];
+                 }
+
+             } // end while (moreLevels)
+
+         }
+
+       // fill in remaining levels with empty DisjointBoxLayouts
+       for (int lev= a_finestLevel+1; lev<=maxLevel; lev++)
+         {
+           a_amrGrids[lev] = DisjointBoxLayout();
+         }
+
+     }
+
+
+}
+
 /*  Setting up the parameters of the solver
 *
 *
